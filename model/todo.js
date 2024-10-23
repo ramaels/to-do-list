2dbd3fde4c5b5bdebaf64f8eb610e7a5d7b4b2c2
Infinite Spiral <stephaneramael@live.co.uk>
2024-10-23 02:56:59 +0100

    Building the model directory

---
+++ model/todo.js
@@ -0,0 +1,17 @@
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+const pool = require('./database');
+
+const create = async (description) => {
+  await pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [description]);
+};
+
+const get = async () => {
+  await pool.query('SELECT * FROM todo RETURNING *');
+};
+
+const remove = async (id) => {
+  await pool.query('DELETE FROM todo WHERE todo_id = $1 RETURNING *', [id]);
+};
+
+module.exports = { create, get, remove };
